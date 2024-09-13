44423b5036b3d6388060964fe5460ef525866de7
Pritam Bala <pritam@localhost.localdomain>
2024-09-13 22:59:55 +0530

    Initial commit

---
+++ src/main/java/io/app/UrbanAttireBackendApplication.java
@@ -0,0 +1,13 @@
+package io.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanAttireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanAttireBackendApplication.class, args);
+	}
+
+}
