ae3b8d5c72ec97cb81df05a5329d53df1b72d344
Jhantu Bala <108252921+jhantu626@users.noreply.github.com>
2024-09-24 14:45:46 +0530

    Update OrderController.java

--- src/main/java/io/app/controller/OrderController.java
+++ src/main/java/io/app/controller/OrderController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/order")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class OrderController {
     private final OrderServiceImpl service;
 
